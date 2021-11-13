e9a52fd75d695ec2257939e8b8e4a10a1fa08b64
safwen-code <safwendjebbi1234@gmail.com>
2021-11-13 23:58:21 +0100

    add kmm info details

--- src/components/content/KmContent/SableuseInfo.jsx
+++ src/components/content/KmContent/SableuseInfo.jsx
@@ -4,13 +4,11 @@ const SableuseInfo = () => {
   return (
     <>
       <div className=" col border border-dark d-flex justify-content-center">
-       aricle name {" "} quantity
+        aricle name quantity
       </div>
       <div className="row g-2 ms-2 mt-2 mb-3">
         <div className="col-6 ">other user</div>
         <div className="col-6 ">other user</div>
-       
-        
       </div>
     </>
   );
