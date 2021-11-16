d9c8332bd5a954ed6fc886d7f320216b513c961e
safwen-code <safwendjebbi1234@gmail.com>
2021-11-16 01:24:46 +0100

    use context to Show kmInfos

--- src/components/content/KmContent/KmInfo.jsx
+++ src/components/content/KmContent/KmInfo.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import KmContent from "./KmContent";
 import KmHeader from "./KmHeader";
 
+import KmContext from '../../../context/kmContext'
+
 const KmInfo = () => {
+  const kmContext = useContext(KmContext)
+  const {kmsInfo} = kmContext
+  console.log('kmsinfo' , kmsInfo)
+
   return (
     <div className="">
       <KmHeader />
